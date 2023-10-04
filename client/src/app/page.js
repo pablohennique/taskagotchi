3ea36d5427b9d775280ece3960c37b9a8036d206
Pablo Hennique <phennique@gmail.com>
2023-10-04 15:38:21 -0400

    first push after resetting git

---
+++ client/src/app/page.js
@@ -0,0 +1,19 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+export default function Home() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+  // test
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
+}
