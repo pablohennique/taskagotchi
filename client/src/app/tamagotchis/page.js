f59891db5e6afbb9796ba1da73392da3faa855ef
Pablo Hennique <phennique@gmail.com>
2023-10-11 12:14:10 -0400

    created custom hook and extracted fetch tamagotchis logic to lib

--- client/src/app/tamagotchis/page.js
+++ client/src/app/tamagotchis/page.js
@@ -1,37 +1,11 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import { useBackendFetchCall } from "@/lib/backend";
 import TamagotchiList from "../../components/tamagotchis-list";
 
 export default function TamagotchisPage() {
-  const [tamagotchis, setTamagotchis] = useState([]);
-  // console.log(`Bearer ${sessionStorage.getItem("accessToken")}`);
-
-  useEffect(() => {
-    const url = "http://localhost:8000/tamagotchis";
-    const options = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
-      },
-    };
-
-    fetch(url, options)
-      .then((res) => {
-        if (res.ok) {
-          return res.json({ message: "User Authenticated" });
-        } else {
-          throw new Error("Login failed");
-        }
-      })
-      .then((data) => {
-        console.log(data);
-        setTamagotchis(data);
-      })
-      .catch((error) => {
-        console.error("An error occurred during login:", error);
-      });
-  }, []);
+  // const [tamagotchis, setTamagotchis] = useState([]);
+  const [tamagotchis, setTamagotchis] = useBackendFetchCall("tamagotchis", []);
 
   return (
     <>
