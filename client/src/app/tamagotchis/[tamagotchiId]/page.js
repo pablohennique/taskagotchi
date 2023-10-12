e1945de724bbc2d9afe50600eb47a0d915ded9a6
Pablo Hennique <phennique@gmail.com>
2023-10-12 14:43:06 -0400

    added tamagotchi gifs and updated delete button description to uppercase

--- client/src/app/tamagotchis/[tamagotchiId]/page.js
+++ client/src/app/tamagotchis/[tamagotchiId]/page.js
@@ -1,6 +1,8 @@
 "use client";
 
 import { useBackendFetchCall } from "@/lib/backend";
+import Link from "next/link";
+import styles from "./page.module.css";
 
 export default function TamagotchiPage({ params }) {
   const url = `http://localhost:8000/tamagotchis/${params.tamagotchiId}`;
@@ -10,18 +12,52 @@ export default function TamagotchiPage({ params }) {
     url
   );
   let hungerDescription;
-  if (tamagotchi.hunger <= 25) {
-    hungerDescription = "Very Hungry";
-  } else if (tamagotchi.hunger <= 50) {
-    hungerDescription = "Hungry";
+  let tamagotchiImage;
+  let tamagotchiNameUppercase;
+  if (tamagotchi.name) {
+    tamagotchiNameUppercase = tamagotchi.name.toUpperCase();
   }
+  function setHungerDescription() {
+    if (tamagotchi.hunger <= 25) {
+      hungerDescription = "Very Hungry";
+    } else if (tamagotchi.hunger <= 50) {
+      hungerDescription = "Hungry";
+    }
+  }
+  function setTamagotchiImage(breed) {
+    switch (breed) {
+      case "Dragon":
+        tamagotchiImage =
+          "/tamagotchis-images/dragon/baby-dragon-tamagotchi.gif";
+        break;
+      case "Dog":
+        tamagotchiImage = "/tamagotchis-images/dog/baby-dog-tamagotchi.gif";
+        break;
+      case "Cat":
+        tamagotchiImage = "/tamagotchis-images/cat/baby-cat-tamagotchi.gif";
+        break;
+    }
+  }
+  setHungerDescription();
+  setTamagotchiImage(tamagotchi.breed);
   return (
     <>
-      <h1>{tamagotchi.name}</h1>
-      <h3>Breed: {tamagotchi.breed}</h3>
-      <h3>Age: {tamagotchi.age}</h3>
-      <h3>Hunger: {hungerDescription}</h3>
-      {/* evolution stage will be showed in the image and will be calculated in Node */}
+      <div className={styles.tamagotchiContainer}>
+        <div>
+          <h1>{tamagotchi.name}</h1>
+          <h3>Breed: {tamagotchi.breed}</h3>
+          <h3>Age: {tamagotchi.age}</h3>
+          <h3>Hunger: {hungerDescription}</h3>
+        </div>
+        <div>
+          {/* evolution stage will be showed in the image and will be calculated in Node */}
+          <img src={tamagotchiImage} alt="Cat Tamagotchi" />
+        </div>
+      </div>
+      <div className="buttonContainer">
+        <Link href="/tamagotchis/create">EDIT TAMAGOTCHI</Link>
+        <Link href="/tamagotchis/create">KILL {tamagotchiNameUppercase}</Link>
+      </div>
     </>
   );
 }
--- client/src/components/tamagotchis-list.js
+++ client/src/components/tamagotchis-list.js
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import styles from "./tamagotchis-list.module.css";
 
 export default function TamagotchisList({ tamagotchis }) {
   return (
-    <ul>
+    <ul className={styles.list}>
       {tamagotchis.map((tamagotchi) => (
         <Link key={tamagotchi._id} href={`/tamagotchis/${tamagotchi._id}`}>
-          <li key={tamagotchi._id}>{tamagotchi.name}</li>
+          <li key={tamagotchi._id}>
+            {tamagotchi.name} - {tamagotchi.breed}
+          </li>
         </Link>
       ))}
     </ul>
